44e01f54b0e1c913dfc6494e6e2d7bc151e92cf1
Nischal Hada <nischal@moroku.com>
2019-07-05 10:05:48 +1000

    Implemented View layer changes

--- EmployeeRoster/View/EmployeeRosterVC.swift
+++ EmployeeRoster/View/EmployeeRosterVC.swift
@@ -11,43 +11,116 @@ import RxSwift
 import RxCocoa
 
 class EmployeeRosterVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView?
+    var activityIndicator: ActivityIndicator? = ActivityIndicator()
+
+    @IBOutlet weak var buttonRefresh: UIBarButtonItem!
     private let disposeBag = DisposeBag()
     var viewModel: EmployeeViewModelProtocol!
-    private var employeeList: EmployeeModel!
-    
+    private var employeeList: EmployeeModel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupUI()
+        self.setupTableView()
         self.viewModel = EmployeeViewModel()
         self.viewModelSetUp()
         self.viewModel.getRosterInfo()
     }
-    
+
+    func setupUI() {
+        self.navigationItem.title = "Roster"
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        self.view.backgroundColor = ThemeColor.white
+    }
+
+    func setupTableView() {
+        self.tableView?.backgroundColor = ThemeColor.tableViewBackgroundColor
+        self.view.backgroundColor = ThemeColor.tableViewBackgroundColor
+        self.tableView?.tableFooterView = UIView(frame: CGRect.zero)
+    }
+
     func viewModelSetUp() {
         self.viewModel.employeeResult
             .subscribe(onNext: { result in
                 self.employeeList = result
-                
+                DispatchQueue.main.async {
+                    self.tableView?.reloadData()
+                }
             }, onError: { (error) in
                 print("error:\(error)")
             }).disposed(by: disposeBag)
-        
+
         self.viewModel
             .errorResult
             .subscribe(onNext: { error in
                 print("error:\(error)")
             })
             .disposed(by: disposeBag)
-        
+
+        self.buttonRefresh.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.viewModel.getRosterInfo()
+            }).disposed(by: disposeBag)
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - TableViewDelegate Setup
+extension EmployeeRosterVC: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 200
+        default:
+            return 150
+        }
     }
-    */
 
+     func numberOfSections(in tableView: UITableView) -> Int {
+        if let _ = self.employeeList {
+            return 2
+        }
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            if let _ = self.employeeList {
+                return 1
+            }
+            return 0
+        default:
+            return self.employeeList?.roster?.count ?? 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "RosterTableViewCell", for: indexPath) as? RosterTableViewCell else {
+                fatalError("RosterTableViewCell does not exist")
+            }
+            cell.dataValue = self.employeeList?.scheduled_today
+            return cell
+
+        default:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeeDetailCell", for: indexPath) as? EmployeeDetailCell else {
+                fatalError("EmployeeDetailCell does not exist")
+            }
+            if
+                let roster = self.employeeList?.roster
+            {
+                cell.dataValue = roster[indexPath.row]
+            }
+            return cell
+        }
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped")
+    }
 }
--- EmployeeRoster/ViewModel/EmployeeViewModel.swift
+++ EmployeeRoster/ViewModel/EmployeeViewModel.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 NischalHada. All rights reserved.
 //
 
-
 import UIKit
 import RxSwift
 import RxCocoa
@@ -16,16 +15,25 @@ class EmployeeViewModel: EmployeeViewModelProtocol {
     private let disposeBag = DisposeBag()
     var employeeResult: Observable<EmployeeModel>
     var errorResult: Observable<Error>
-    
+
     private let employeeResultSubject = PublishSubject<EmployeeModel>()
     private let errorResultSubject = PublishSubject<Error>()
-    
+
     init(withGetWeather getEmployeeHandler: GetEmployeeInfoHandlerProtocol = GetEmployeeInfoHandler()) {
         self.getEmployeeHandler = getEmployeeHandler
         self.employeeResult = employeeResultSubject.asObserver()
         self.errorResult = errorResultSubject.asObserver()
+        self.reloadTask()
     }
-    
+
+    private func reloadTask() {
+        let scheduler = SerialDispatchQueueScheduler(qos: .default)
+        Observable<Int>.interval(.seconds(200), scheduler: scheduler)
+            .subscribe { [weak self] _ in
+                self?.getRosterInfo()
+            }.disposed(by: disposeBag)
+    }
+
     func getRosterInfo() {
         self.getEmployeeHandler.request()
             .retry(3)
@@ -43,5 +51,3 @@ class EmployeeViewModel: EmployeeViewModelProtocol {
             .disposed(by: disposeBag)
     }
 }
-
-
