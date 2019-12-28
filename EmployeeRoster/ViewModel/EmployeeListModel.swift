93880330d05a4c0b9ef7597d62642e4bba6b4823
Nischal Hada <hadanischal@gmail.com>
2019-12-29 10:49:28 +1100

    Implemented EmployeeListModel and EmployeeListModelTests

---
+++ EmployeeRoster/ViewModel/EmployeeListModel.swift
@@ -0,0 +1,57 @@
+//
+//  EmployeeListModel.swift
+//  EmployeeRoster
+//
+//  Created by Nischal Hada on 28/12/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import CocoaLumberjack
+
+protocol EmployeeListModelDataSource {
+    func getRosterInfo() -> Observable<EmployeeModel>
+}
+
+final class EmployeeListModel: EmployeeListModelDataSource {
+
+    private let getEmployeeHandler: GetEmployeeInfoHandlerProtocol
+    private let realmManager: RealmManagerDataSource
+    private let disposeBag = DisposeBag()
+
+    init(withGetWeather getEmployeeHandler: GetEmployeeInfoHandlerProtocol = GetEmployeeInfoHandler(),
+         withRealmManager realmManager: RealmManagerDataSource = RealmManager()) {
+        self.getEmployeeHandler = getEmployeeHandler
+        self.realmManager = realmManager
+    }
+
+    func getRosterInfo() -> Observable<EmployeeModel> {
+        let result = Observable.concat(getEmployeeInfoFromLocalDb(), getEmployeeInfoFromServer())
+        return result
+    }
+    // Get employee roster from server
+    func getEmployeeInfoFromServer() -> Observable<EmployeeModel> {
+        self.getEmployeeHandler
+            .request()
+            .retry(3)
+            .catchError { _ in Observable.empty()}
+            .flatMap { [weak self] result -> Observable<EmployeeModel> in
+                guard let self = self else { return Observable.empty() }
+                return self.saveEmployeeInfo(withInfo: result)
+                    .andThen(Observable.just(result))
+        }
+    }
+
+    // Get employee roster from DB
+    func getEmployeeInfoFromLocalDb() -> Observable<EmployeeModel> {
+        realmManager.fetchEmployeeInfo().asObservable()
+            .catchError { _ in Observable.empty() }
+    }
+
+    private func saveEmployeeInfo(withInfo data: EmployeeModel) -> Completable {
+        return self.realmManager.saveEmployeeInfo(withInfo: data)
+            .catchError { _ in Completable.empty()}
+    }
+
+}
