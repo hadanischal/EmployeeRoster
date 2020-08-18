631ed441e9858bbdc53b0a44d225df4af4e403cd
Nischal Hada <hadanischal@gmail.com>
2020-08-18 20:43:12 +1000

    [Extension] Added UITableView and Bundle Extension

---
+++ EmployeeRoster/Utility/Extension/Bundle+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Bundle+Extension.swift
+//  EmployeeRoster
+//
+//  Created by Nischal Hada on 13/10/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    var displayName: String? {
+        return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+}
