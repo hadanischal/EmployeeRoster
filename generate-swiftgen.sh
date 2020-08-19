e6ca7bf3e2e68a44ccef2493a2673ea6fe33a7ae
Nischal Hada <hadanischal@gmail.com>
2020-08-19 22:28:29 +1000

    [Swiftgen] Added SwiftGen setup file

---
+++ generate-swiftgen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  generate-swiftgen.sh
+#  OpenJobs
+#
+#  Created by Nischal Hada on 25/11/19.
+#  Copyright © 2019 Nischal Hada. All rights reserved.
+PROJECT_DIR="."
+PODS_ROOT="$PROJECT_DIR/Pods"
+
+${PODS_ROOT}/SwiftGen/bin/swiftgen
