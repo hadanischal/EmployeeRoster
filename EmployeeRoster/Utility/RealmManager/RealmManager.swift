05ac68a1187631d391a30bb2b1be65ea6d6f25cc
Nischal Hada <hadanischal@gmail.com>
2020-08-20 23:36:08 +1000

    [RealmManager] Added RealmManager and  RealmManagerTests

---
+++ EmployeeRoster/Utility/RealmManager/RealmManager.swift
@@ -0,0 +1,109 @@
+//
+//  RealmManager.swift
+//  EmployeeRoster
+//
+//  Created by Nischal Hada on 6/10/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import CocoaLumberjack
+import Foundation
+import RealmSwift
+import RxSwift
+
+protocol RealmManagerDataSource {
+    func saveEmployeeInfo(withInfo data: EmployeeModel) -> Completable
+    func fetchEmployeeInfo() -> Single<EmployeeModel>
+}
+
+struct RealmManager: RealmManagerDataSource {
+    private var queueScheduler: SchedulerType!
+
+    init(withQueueScheduler queueScheduler: SchedulerType = ConcurrentDispatchQueueScheduler(qos: DispatchQoS.background)) {
+        self.queueScheduler = queueScheduler
+    }
+
+    /// clear and save employee info to DB
+    func saveEmployeeInfo(withInfo info: EmployeeModel) -> Completable {
+        return deleteEmployeeInfo()
+        .andThen(saveInfo(withInfo: info))
+    }
+
+    /// Save employee info to DB
+   private func saveInfo(withInfo data: EmployeeModel) -> Completable {
+        return Completable.create { completable in
+            do {
+                let realm = try Realm()
+                let realmEmployee = RealmEmployee()
+
+                // ScheduleModel
+                let realmSchedule = RealmSchedule()
+                if let scheduledToday = data.scheduledToday {
+                    realmSchedule.update(withScheduleModel: scheduledToday)
+                    realmEmployee.scheduledToday = realmSchedule
+                }
+
+                // RosterModel
+                if let roster = data.roster {
+                    _ = roster.map({ rosterModel -> RealmRoster in
+                        let realmRoster = RealmRoster()
+                        realmRoster.update(withRosterModel: rosterModel)
+                        realmEmployee.roster.append(realmRoster)
+                        return realmRoster
+                    })
+                }
+                try realm.write {
+                    realmEmployee.update(withEmployeeModel: data)
+                    realm.add(realmEmployee)
+                }
+                completable(.completed)
+                return Disposables.create {}
+            } catch {
+                DDLogError("Realm error: \(error)")
+                completable(.error(error))
+                return Disposables.create {}
+            }
+    }.subscribeOn(ConcurrentDispatchQueueScheduler(qos: DispatchQoS.default))
+    }
+
+    /// get employee info from DB
+    func fetchEmployeeInfo() -> Single<EmployeeModel> {
+        return Single<EmployeeModel>.create { single in
+            do {
+                let realm = try Realm()
+                guard let realmEmployee = realm.objects(RealmEmployee.self).first else {
+                    single(.error(RxError.noElements))
+                    return Disposables.create {}
+                }
+
+                let result = EmployeeModel(withRealmEmployee: realmEmployee)
+                DDLogInfo("realmEmployee: \(realmEmployee)")
+                single(.success(result))
+
+            } catch {
+                DDLogError("Realm error: \(error)")
+                single(.error(error))
+            }
+            return Disposables.create {}
+            }.subscribeOn(self.queueScheduler)
+    }
+
+    /// clear employee info from DB
+    private func deleteEmployeeInfo() -> Completable {
+        return Completable.create { completable in
+            do {
+                let realm = try Realm()
+                let realmEmployee = realm.objects(RealmEmployee.self)
+                try realm.write {
+                    realm.delete(realmEmployee)
+                }
+                completable(.completed)
+                return Disposables.create {}
+            } catch {
+                DDLogError("Realm error: \(error)")
+                completable(.error(error))
+                return Disposables.create {}
+            }
+            }.subscribeOn(ConcurrentDispatchQueueScheduler(qos: DispatchQoS.background))
+    }
+}
---
+++ EmployeeRosterTests/RealmManagerTests.swift
@@ -0,0 +1,62 @@
+//
+//  RealmManagerTests.swift
+//  EmployeeRosterTests
+//
+//  Created by Nischal Hada on 11/10/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import Cuckoo
+import Nimble
+import Quick
+import RxBlocking
+import RxSwift
+import RxTest
+import XCTest
+
+@testable import EmployeeRoster
+
+class RealmManagerTests: QuickSpec {
+    override func spec() {
+        var testHandler: RealmManager!
+        var testScheduler: TestScheduler!
+        let employeeModel: EmployeeModel = MockData().stubEmployeeModel() ?? EmployeeModel.empty
+
+        describe("RealmManager") {
+            beforeEach {
+                testScheduler = TestScheduler(initialClock: 0)
+                testHandler = RealmManager(withQueueScheduler: MainScheduler.instance)
+            }
+
+            describe("when save Employee List to local DB succeed ", {
+                var result: MaterializedSequenceResult<Never>?
+                beforeEach {
+                    result = testHandler.saveEmployeeInfo(withInfo: employeeModel).toBlocking().materialize()
+                }
+                it("it completed successfully", closure: {
+                    result?.assertSequenceCompletes()
+                })
+            })
+
+            describe("when get jobs Closed Jobs List from local DB succeed ", {
+                var result: MaterializedSequenceResult<Never>?
+
+                beforeEach {
+                    result = testHandler.saveEmployeeInfo(withInfo: employeeModel).toBlocking().materialize()
+                }
+                it("it completed successfully", closure: {
+                    result?.assertSequenceCompletes()
+                })
+                it("return the news list from DB", closure: {
+                    let observable = testHandler.fetchEmployeeInfo().asObservable()
+
+                    let res = testScheduler.start { observable }
+                    expect(res.events.count).to(equal(2))
+                    let correctResult = [Recorded.next(200, employeeModel),
+                                         Recorded.completed(200)]
+                    expect(res.events).to(equal(correctResult))
+                })
+            })
+        }
+    }
+}
