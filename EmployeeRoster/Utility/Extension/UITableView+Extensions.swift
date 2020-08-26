3ec11ea8c4c932d1c2c51b1b2590a703e7fa5611
Nischal Hada <hadanischal@gmail.com>
2020-08-26 18:13:40 +1000

    [UITableView+Extensions] Updated UITableView+Extensions

--- EmployeeRoster/Utility/Extension/UITableView+Extensions.swift
+++ EmployeeRoster/Utility/Extension/UITableView+Extensions.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 extension UITableView {
+    func register<T: UITableViewCell>(_: T.Type) {
+        let nib = UINib(nibName: T.nibName, bundle: nil)
+        register(nib, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+
     func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
@@ -20,7 +25,7 @@ extension UITableView {
 
 extension UITableView {
     func hideEmptyCells() {
-        self.tableFooterView = UIView(frame: .zero)
+        tableFooterView = UIView(frame: .zero)
     }
 
     func scrollToTopRow() {
